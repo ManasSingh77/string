8b3ec8313578915cb0de8d9698872eee5d749c4f
Manas Singh <122530839+ManasSingh77@users.noreply.github.com>
2023-09-16 18:09:55 +0530

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,12 @@
+package string;
+
+public class replace {
+
+	public static void main(String[] args) {
+		String text="Manas singh is a good boy";
+		text=text.replace(" ","_");
+		System.out.println(text);
+
+	}
+
+}
---
+++ string.java
@@ -0,0 +1,12 @@
+package string;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Manas Singh";
+		name=name.toLowerCase();
+		System.out.println(name);
+	}
+
+}
